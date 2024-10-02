9cbaf083d1ec5dbb70205067ccf63126e3e3872c
CloudStoned <cloudneo0719@gmail.com>
2024-10-02 08:45:20 +0800

    Remove dotenv

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-import os 
-from dotenv import load_dotenv
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
